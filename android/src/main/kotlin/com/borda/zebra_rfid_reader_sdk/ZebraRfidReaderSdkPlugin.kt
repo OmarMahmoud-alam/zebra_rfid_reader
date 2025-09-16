ae4739417ea2cac5ad9dfd790ba7315b4a9cb5a3
Omar <eng.omar.control@gmail.com>
2025-09-16 12:42:52 +0300

    try1

--- android/src/main/kotlin/com/borda/zebra_rfid_reader_sdk/ZebraRfidReaderSdkPlugin.kt
+++ android/src/main/kotlin/com/borda/zebra_rfid_reader_sdk/ZebraRfidReaderSdkPlugin.kt
@@ -11,19 +11,9 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.BinaryMessenger // Import BinaryMessenger
-import io.flutter.plugin.common.PluginRegistry.Registrar // Import Registrar
-import android.content.Context // Import Context
 
 /** ZebraRfidReaderSdkPlugin */
 class ZebraRfidReaderSdkPlugin : FlutterPlugin, MethodCallHandler {
-    companion object {
-        @JvmStatic
-        fun register(registrar: Registrar) {
-            val plugin = ZebraRfidReaderSdkPlugin()
-            plugin.initialize(registrar.messenger(), registrar.context())
-        }
-    }
     /// The MethodChannel that will the communication between Flutter and native Android
     ///
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
@@ -49,123 +39,153 @@ class ZebraRfidReaderSdkPlugin : FlutterPlugin, MethodCallHandler {
     }
 
     override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        Log.d(LOG_TAG, "Calllllllledd:::::::")
-        methodChannel =
-            MethodChannel(flutterPluginBinding.binaryMessenger, "borda/zebra_rfid_reader_sdk")
+        Log.d(LOG_TAG, "Plugin attached to engine - initializing all components")
+        
+        // Setup Method Channel
+        methodChannel = MethodChannel(flutterPluginBinding.binaryMessenger, "borda/zebra_rfid_reader_sdk")
         methodChannel.setMethodCallHandler(this)
 
+        // Setup Event Channels
         tagHandlerEvent = EventChannel(flutterPluginBinding.binaryMessenger, "tagHandlerEvent")
         tagFindingEvent = EventChannel(flutterPluginBinding.binaryMessenger, "tagFindingEvent")
         readTagEvent = EventChannel(flutterPluginBinding.binaryMessenger, "readTagEvent")
 
+        // Create Event Handlers
         tagDataEventHandler = TagDataEventHandler()
         tagFindingEventHandler = TagDataEventHandler()
         readTagEventHandler = TagDataEventHandler()
 
-
-        tagHandlerEvent.setStreamHandler(tagDataEventHandler)
-        tagFindingEvent.setStreamHandler(tagFindingEventHandler)
-        readTagEvent.setStreamHandler(readTagEventHandler)
-        Log.d(LOG_TAG, "onAttachedToEngine called")
-        connectionHelper = ZebraConnectionHelper(flutterPluginBinding.applicationContext, tagDataEventHandler, tagFindingEventHandler, readTagEventHandler)
-        connectionHelperInitializationListener?.onConnectionHelperInitialized() // Notify
-    }
-
-
-
-    private fun initialize(messenger: BinaryMessenger, context: Context) {
-        Log.d(LOG_TAG, "Calllllllledd:::::::")
-        methodChannel = MethodChannel(messenger, "borda/zebra_rfid_reader_sdk")
-        methodChannel.setMethodCallHandler(this)
-
-        tagHandlerEvent = EventChannel(messenger, "tagHandlerEvent")
-        tagFindingEvent = EventChannel(messenger, "tagFindingEvent")
-        readTagEvent = EventChannel(messenger, "readTagEvent")
-
-        tagDataEventHandler = TagDataEventHandler()
-        tagFindingEventHandler = TagDataEventHandler()
-        readTagEventHandler = TagDataEventHandler()
-
+        // Connect Event Handlers to Channels
         tagHandlerEvent.setStreamHandler(tagDataEventHandler)
         tagFindingEvent.setStreamHandler(tagFindingEventHandler)
         readTagEvent.setStreamHandler(readTagEventHandler)
 
-        Log.d(LOG_TAG, "registerWith called")
-        connectionHelper = ZebraConnectionHelper(context, tagDataEventHandler, tagFindingEventHandler, readTagEventHandler)
+        Log.d(LOG_TAG, "Channels and handlers initialized - creating connection helper")
+        
+        // Initialize Connection Helper with application context
+        connectionHelper = ZebraConnectionHelper(
+            flutterPluginBinding.applicationContext, 
+            tagDataEventHandler, 
+            tagFindingEventHandler, 
+            readTagEventHandler
+        )
+        
+        // Notify that initialization is complete
         connectionHelperInitializationListener?.onConnectionHelperInitialized()
+        Log.d(LOG_TAG, "Plugin initialization completed successfully")
     }
 
     override fun onMethodCall(call: MethodCall, result: Result) =
         when (call.method) {
             "connect" -> {
-                Log.d(LOG_TAG, "connect called")
+                Log.d(LOG_TAG, "connect method called")
                 val name = call.argument<String>("name")!!
                 val readerConfig = call.argument<HashMap<String, Any>>("readerConfig")!!
-                Log.d(LOG_TAG, "will try to connect to -> $name")
-                Log.d(LOG_TAG, "USER CONFIG -> $readerConfig")
+                Log.d(LOG_TAG, "Attempting to connect to reader: $name")
+                Log.d(LOG_TAG, "Reader configuration: $readerConfig")
                 connectionHelper.connect(name, readerConfig)
             }
 
             "disconnect" -> {
-                Log.d(LOG_TAG, "disconnect called")
+                Log.d(LOG_TAG, "disconnect method called")
                 connectionHelper.disconnect()
             }
 
             "setAntennaPower" -> {
+                Log.d(LOG_TAG, "setAntennaPower method called")
                 val transmitPowerIndex = call.argument<Int>("transmitPowerIndex")!!
+                Log.d(LOG_TAG, "Setting antenna power to index: $transmitPowerIndex")
                 connectionHelper.setAntennaConfig(transmitPowerIndex)
             }
 
             "setDynamicPower" -> {
+                Log.d(LOG_TAG, "setDynamicPower method called")
                 val isEnable = call.argument<Boolean>("isEnable")!!
+                Log.d(LOG_TAG, "Setting dynamic power enabled: $isEnable")
                 connectionHelper.setDynamicPower(isEnable)
             }
 
             "setBeeperVolume" -> {
+                Log.d(LOG_TAG, "setBeeperVolume method called")
                 val level = call.argument<Int>("level")!!
+                Log.d(LOG_TAG, "Setting beeper volume to level: $level")
                 connectionHelper.setBeeperVolumeConfig(level)
             }
 
             "findTheTag" -> {
+                Log.d(LOG_TAG, "findTheTag method called")
                 val tag = call.argument<String>("tag")!!
-                Log.d("ENGIN", "findTheTag called with tag -> $tag")
+                Log.d(LOG_TAG, "Starting tag search for: $tag")
                 connectionHelper.findTheTag(tag)
             }
 
             "stopFindingTheTag" -> {
+                Log.d(LOG_TAG, "stopFindingTheTag method called")
                 connectionHelper.stopFindingTheTag()
             }
 
             "getAvailableReaderList" -> {
-                if (::connectionHelper.isInitialized) { // Check for initialization
+                Log.d(LOG_TAG, "getAvailableReaderList method called")
+                if (::connectionHelper.isInitialized) {
                     getAvailableReaderList(result)
                 } else {
+                    Log.e(LOG_TAG, "Connection helper not initialized when getAvailableReaderList was called")
                     result.error("CONNECTION_HELPER_NOT_INITIALIZED", "Connection Helper not initialized yet", null)
                 }
             }
-            else -> result.notImplemented()
+            
+            else -> {
+                Log.w(LOG_TAG, "Unknown method called: ${call.method}")
+                result.notImplemented()
+            }
         }
 
     private fun getAvailableReaderList(result: Result) {
-        Log.d(LOG_TAG, "getAvailableReaderList called")
-        val readers = connectionHelper.getAvailableReaderList()
-        val dataList = mutableListOf<BordaReaderDevice>()
-        for (reader in readers) {
-            val device = BordaReaderDevice(
+        Log.d(LOG_TAG, "Retrieving available reader list")
+        try {
+            val readers = connectionHelper.getAvailableReaderList()
+            val dataList = mutableListOf<BordaReaderDevice>()
+            
+            for (reader in readers) {
+                val device = BordaReaderDevice(
                     ConnectionStatus.notConnected,
                     reader.name.toString(),
                     null,
                     null
-            )
-            dataList.add(device)
+                )
+                dataList.add(device)
+            }
+            
+            val jsonResult = Gson().toJson(dataList)
+            Log.d(LOG_TAG, "Found ${dataList.size} available readers")
+            result.success(jsonResult)
+        } catch (e: Exception) {
+            Log.e(LOG_TAG, "Error getting available reader list: ${e.message}", e)
+            result.error("READER_LIST_ERROR", "Failed to get available readers: ${e.message}", null)
         }
-        result.success(Gson().toJson(dataList))
     }
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        Log.d(LOG_TAG, "Plugin detaching from engine - starting cleanup")
+        
+        // Clean up method channel
         methodChannel.setMethodCallHandler(null)
+        
+        // Clean up event channels
         tagHandlerEvent.setStreamHandler(null)
         tagFindingEvent.setStreamHandler(null)
-        connectionHelper.dispose()
+        readTagEvent.setStreamHandler(null)
+        
+        // Clean up connection helper
+        if (::connectionHelper.isInitialized) {
+            try {
+                connectionHelper.dispose()
+                Log.d(LOG_TAG, "Connection helper disposed successfully")
+            } catch (e: Exception) {
+                Log.e(LOG_TAG, "Error disposing connection helper: ${e.message}", e)
+            }
+        }
+        
+        Log.d(LOG_TAG, "Plugin cleanup completed")
     }
 }
